9d798fc9098467bb6186b857a99b4e540554789c
charlesjamb <charles.jambon@gmail.com>
2016-11-13 17:47:51 -0500

    able to get token from user!

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const logger = require('morgan');
+const session = require('express-session');
+
+const Grant = require('grant-express');
+const grant = new Grant(require('./config.json'));
+
 const app = express();
+app.use(logger('dev'));
+app.use(session({secret: 'very secret'}));
+app.use(grant);
 
-app.use('/files', express.static(__dirname + '/src'));
-
-app.get('/*', function(request, response) {
-  response.sendFile(__dirname + '/src/index.html');
+app.get('/handle_facebook_callback', function(req, res) {
+	console.log(req.query)
+	res.end(JSON.stringify(req.query, null, 2))
 });
 
-app.listen(process.env.PORT || 8080, function() {
-  console.log('Server started');
+app.listen(8080, function () {
+	console.log('Server started on port: ' + 8080);
 });
+
+// React
+// app.use('/files', express.static(__dirname + '/src'));
+
+// app.get('/*', function(request, response) {
+//   response.sendFile(__dirname + '/src/index.html');
+// });
